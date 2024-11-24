d5cea9583ab594b167bdbaeb835c2499955c8827
jiikko <n905i.1214@gmail.com>
2024-11-24 22:46:44 +0900

    first commit

---
+++ lib/puma/plugin/shellscript.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative 'shellscript/version'
+
+Puma::Plugin.create do
+  def start(launcher)
+    @child_pid = nil
+
+    launcher.events.register(:state) do |state|
+      if %i[halt restart stop].include?(state)
+        if @child_pid
+          puts "[puma plugin command] Killing child process #{@child_pid}"
+          begin
+            Process.kill('TERM', @child_pid)
+            Process.wait(@child_pid)
+          rescue Errno::ESRCH, Errno::ECHILD => e
+            puts "[puma plugin command] Failed to kill process: #{e.message}"
+          ensure
+            @child_pid = nil
+          end
+        else
+          puts '[puma plugin command] No child process to kill'
+        end
+      end
+    end
+
+    command = ENV['PUMA_PLUGIN_COMMAND']
+
+    in_background do
+      loop do
+        puts "[puma plugin command] Running command: #{command}"
+        stdout, _stdin, pid = PTY.spawn(command)
+        @child_pid = pid
+
+        puts stdout.map(&:itself).join
+        _, status = Process.wait2(pid)
+        if status.success?
+          puts '[puma plugin command] Command exited successfully'
+        else
+          puts "[puma plugin command] Command failed with status: #{status.exitstatus}"
+        end
+      rescue Errno::EIO => e
+        Rails.logger.error "[puma plugin command] Error: #{e}"
+      ensure
+        @child_pid = nil
+      end
+    end
+  end
+end
---
+++ spec/puma/plugin/shellscript_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Puma::Plugin::Shellscript do
+  it "has a version number" do
+    expect(Puma::Plugin::Shellscript::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

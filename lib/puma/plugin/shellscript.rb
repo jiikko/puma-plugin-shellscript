21ca91fb7c06fbcff142ed6bc72b0f82aff2fd8b
jiikko <n905i.1214@gmail.com>
2024-11-24 22:54:51 +0900

    rename

--- lib/puma/plugin/shellscript.rb
+++ lib/puma/plugin/shellscript.rb
@@ -10,38 +10,38 @@ Puma::Plugin.create do
     launcher.events.register(:state) do |state|
       if %i[halt restart stop].include?(state)
         if @child_pid
-          puts "[puma plugin command] Killing child process #{@child_pid}"
+          puts "[puma plugin shellscript] Killing child process #{@child_pid}"
           begin
             Process.kill('TERM', @child_pid)
             Process.wait(@child_pid)
           rescue Errno::ESRCH, Errno::ECHILD => e
-            puts "[puma plugin command] Failed to kill process: #{e.message}"
+            puts "[puma plugin shellscript] Failed to kill process: #{e.message}"
           ensure
             @child_pid = nil
           end
         else
-          puts '[puma plugin command] No child process to kill'
+          puts '[puma plugin shellscript] No child process to kill'
         end
       end
     end
 
-    command = ENV['PUMA_PLUGIN_COMMAND']
+    shellscript = ENV['PUMA_PLUGIN_SHELLSCRIPT']
 
     in_background do
       loop do
-        puts "[puma plugin command] Running command: #{command}"
-        stdout, _stdin, pid = PTY.spawn(command)
+        puts "[puma plugin shellscript] Running shellscript: #{shellscript}"
+        stdout, _stdin, pid = PTY.spawn(shellscript)
         @child_pid = pid
 
         puts stdout.map(&:itself).join
         _, status = Process.wait2(pid)
         if status.success?
-          puts '[puma plugin command] Command exited successfully'
+          puts '[puma plugin shellscript] Command exited successfully'
         else
-          puts "[puma plugin command] Command failed with status: #{status.exitstatus}"
+          puts "[puma plugin shellscript] Command failed with status: #{status.exitstatus}"
         end
       rescue Errno::EIO => e
-        Rails.logger.error "[puma plugin command] Error: #{e}"
+        Rails.logger.error "[puma plugin shellscript] Error: #{e}"
       ensure
         @child_pid = nil
       end
